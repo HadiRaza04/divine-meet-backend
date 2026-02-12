e6042b100b852f46a202cfcdc93ff238613b1291
Hadi Raza <hadiraza.edu@gmail.com>
2026-02-12 17:37:22 +0500

    add protected

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { Signup, Signin, ForgotPassword, ResetPassword } from '../controllers/UserController.js';
+import Protect from '../middlewares/Protect.js';
+import authorize from '../middlewares/Role.js';
 
 const UserRouter = express.Router();
 
 
-UserRouter.post('/signup', Signup)
+UserRouter.post('/signup', Protect, Signup)
 UserRouter.post('/signin', Signin)
-UserRouter.post('/reset-password', ForgotPassword)
-UserRouter.post('/reset-password/:token', ResetPassword)
+UserRouter.post('/reset-password', Protect, ForgotPassword)
+UserRouter.post('/reset-password/:token', Protect, ResetPassword)
 
 export default UserRouter;
